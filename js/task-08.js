7342558f4b13e5b7da695d2a8ff56e38240134ba
zavaluschka <38804687+zavaluschka@users.noreply.github.com>
2023-08-03 14:53:28 +0300

    home003

---
+++ js/task-08.js
@@ -0,0 +1,15 @@
+
+
+const form = document.querySelector(".login-form");
+ form.addEventListener("submit", onSubmit)
+function onSubmit(e) {
+    e.preventDefault();
+  const obj = new Object();
+  obj.email = form.elements.email.value;
+  obj.password = form.elements.password.value;
+  console.log(`Email: ${obj.email} Password: ${obj.password}`)
+  
+
+     if(obj.email ===""||obj.password==="") {alert('Всі поля повинні бути заповнені!')}
+   form.reset()
+  }
